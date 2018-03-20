3161ba410a4ea0c9c6fb8b52a8229037f1bb2e5d
ms <ms@sapoto.it>
2018-03-21 00:49:29 +0100

    no message

---
+++ app/components/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function App() {
+    return <div>WOW...Your React Node app is set up!</div>
+};
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    devtool: 'inline-source-map',
+    entry: ['./app/index.js'],
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.join(__dirname, 'app'),
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015', 'react']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './app/public/index.html',
+            filename: 'index.html',
+            inject: 'body',
+        }),
+    ],
+};
